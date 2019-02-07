c0744c563745161766a4e8275a6a59efd26fbeda
Brian Simonton <bsimonton78@gmail.com>
2019-02-07 20:55:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-  elseif string == string.upcase
+  elsif string == string.upcase
     return "NO NOT SINCE 1938!"
    
   else
